2dbdd78b43f2cf2ebe52dd9c0c822035475556b1
Evelyn <130219194+EveyZeram@users.noreply.github.com>
2023-10-25 19:38:49 +0000

    egmexdone

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  // La tarea trata de plasmar una carta donde cada vez
+  // refresques se cambie el número y el símbolo de esa carta
+  //  Primero creo las variables uqe me den esos numeros y esos símbolos
+  let numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  let symbols = ["♦", "♥", "♠", "♣"];
+  let symbolsColors = { "♠": "black", "♦": "red", "♥": "red", "♣": "black" };
+  // Luego necesito que me extraiga números/símbolos rándom a partir de esas variables
+  let randomNumbers = numbers[Math.floor(Math.random() * numbers.length)];
+  let randomSymbols = symbols[Math.floor(Math.random() * symbols.length)];
+  // Conecto con el html el resultado (textcontent) que es randomNumber/Symbols para que se muestre en el ID correspondiente del HTML
+  document.getElementById("numberCenter").textContent = randomNumbers;
+  document.getElementById("symbolSide").textContent = randomSymbols;
+  document.getElementById("symbolSide2").textContent = randomSymbols;
+
+  // Asociar el color del símbolo con el símbolo, desde la variable LET. Para ello necesito
+  let color = symbolsColors[randomSymbols];
+  document.getElementById("symbolSide").style.color = color;
+  document.getElementById("symbolSide2").style.color = color;
 };
